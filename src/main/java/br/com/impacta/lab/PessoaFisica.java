a2c8727f577bae1a011ca01952372c8fa9a81c6d
Danilo Veras <dsveras@outlook.com>
2021-06-05 09:29:02 -0300

    Atividade Interfaces - Atualizada

---
+++ src/main/java/br/com/impacta/lab/PessoaFisica.java
@@ -0,0 +1,34 @@
+package br.com.impacta.lab;
+
+public class PessoaFisica implements Pessoa{
+
+  private String documento;
+  private String tipo;
+
+  public PessoaFisica(String documento, String tipo) {
+    this.documento  = documento;
+    this.tipo = tipo;
+  }
+
+  public String getDocumento() {
+    return this.documento;
+  }
+
+  public void setDocumento(String documento) {
+    this.documento = documento;
+  }
+  public void setTipo(String tipo) {
+    this.tipo = tipo;
+  }
+
+  @Override
+  public String falarDocumento() {
+    return documento;
+  }
+
+  @Override
+  public String getTipo() {    
+    return tipo;
+  }
+  
+}
